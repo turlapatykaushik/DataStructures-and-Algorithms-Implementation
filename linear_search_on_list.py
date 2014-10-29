492901b1ad76b31366f83764e060f76c7b5f96c4
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-10-30 00:53:57 +0530

    Update linear_search_on_list.py

--- linear_search_on_list.py
+++ linear_search_on_list.py
@@ -1,6 +1,8 @@
 /*
 * @turlapatykaushik
 * github url : github.com/turlapatykaushik
+
+* problem description : implementation of linear search on a list in python
 */
 l = [1,4,23,43,21,32,25,41,6,423]
 for i in l:
