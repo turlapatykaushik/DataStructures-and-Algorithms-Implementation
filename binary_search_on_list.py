6a420f1b5a8c0691ccb84958976950d2b7183541
turlapatykaushik <turlapatykaushik95@gmail.com>
2014-10-29 23:36:49 +0530

    Create binary_search_on_list.py

---
+++ binary_search_on_list.py
@@ -0,0 +1,26 @@
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+*/
+
+def binarysearch(youritem,yourlist):
+  found = False
+  bottom = 0
+  top = len(yourlist)-1
+  while bottom<=top and not found:
+    middle = (top+bottom)/2
+    if yourlist[middle]==youritem:
+      found = True
+    elif yourlist[middle] < youritem:
+      bottom = middle + 1
+    else:
+      top = middle-1
+  return found
+yourlist=[2,3,6,7,8,34,65,76,86,95,97,99]
+isitfound = binarysearch(736,yourlist)
+if isitfound:
+  print "yo man! found it"
+else:
+  print "no man! failed to find it"
+
+    
