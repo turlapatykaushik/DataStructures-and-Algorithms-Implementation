6df36d69028d7fd97229ca46d7be2bdfe2eb3278
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-05 21:29:44 +0530

    Update mergesort.c

--- sorting/mergesort.c
+++ sorting/mergesort.c
@@ -1,6 +1,8 @@
-//Name : T.Kaushik
-//Sorting : Merge Sort
-//Roll no : IMT2013054
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+*/
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
