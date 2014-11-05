1459be2e5c75b0543fcf7ebdc0ae6c5c932e9a1b
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-05 21:28:47 +0530

    Update bucketsort.c

--- sorting/bucketsort.c
+++ sorting/bucketsort.c
@@ -1,6 +1,8 @@
-//Name : T.Kaushik
-//Sorting : Bucket Sort
-//Roll no : IMT2013054
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+*/
+
 #include <stdio.h>
 #include<stdlib.h>
 typedef struct nodeobject
