a875ce3d09e66fb4c9403b05e263542c75e7f8d0
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-05 21:30:03 +0530

    Update quicksort.c

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,6 +1,8 @@
-//Name : T.Kaushik
-//Sorting : Quick Sort
-//Roll no : IMT2013054
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+*/
+
 #include<stdio.h>
 void quicksort(long long int [], int , int);
 int main()
